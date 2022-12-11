c68ef29072f15bdaf56b8aa9acc420ff47ce60ed
slaven92 <slaven92@gmail.com>
2022-12-11 19:08:53 +0100

    bugfix: manifest

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,12 +9,12 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My blog`,
+    title: `Slaven's blog`,
     author: {
       name: `Slaven Tepsic`,
       summary: `who lives and works in Barcelona.`,
     },
-    description: `A blog about various things`,
+    description: `A blog about food, coding and other things.`,
     siteUrl: `https://blog.slaven92.com/`,
     social: {
       twitter: `slaven992`,
@@ -111,14 +111,13 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Slaven's Blog`,
-        short_name: `Gatsby`,
+        short_name: `Slaven's Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
+        // cache_busting_mode: 'none',
       },
     },
   ],
